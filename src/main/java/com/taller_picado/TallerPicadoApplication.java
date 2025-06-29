418fa782297884d07fa2216d95838a1711db21da
Heiner Calderón <heinner1124@gmail.com>
2025-06-29 12:50:14 -0600

    Initial commit

---
+++ src/main/java/com/taller_picado/TallerPicadoApplication.java
@@ -0,0 +1,13 @@
+package com.taller_picado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerPicadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerPicadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taller_picado/TallerPicadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller_picado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerPicadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
